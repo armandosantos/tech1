79c1265143296b0ada1e5f8af79b33fc6defc278
armandosantos <rmando.santos@gmail.com>
2016-10-13 00:16:38 -0700

    missing files

---
+++ Classy.py
@@ -0,0 +1,58 @@
+"""You can use this class to represent how classy someone
+or something is.
+"Classy" is interchangable with "fancy".
+If you add fancy-looking items, you will increase
+your "classiness".
+Create a function in "Classy" that takes a string as
+input and adds it to the "items" list.
+Another method should calculate the "classiness"
+value based on the items.
+The following items have classiness points associated
+with them:
+"tophat" = 2
+"bowtie" = 4
+"monocle" = 5
+Everything else has 0 points.
+Use the test cases below to guide you!"""
+
+class Classy(object):
+    def __init__(self):
+        self.items = []
+        self.classiness = 0
+        
+    def detClassiness(item):
+        if "tophat" == item
+            return 2
+        else if "bowtie" == item
+            return 4
+        else if "monocle" == item
+            return 5
+        else
+            return 0
+        
+    def addItem(item):
+        self.items.append(item)
+        self.classiness += detClassiness(item)
+        
+    def getClassiness(self):
+        return self.classiness
+
+# Test cases
+me = Classy()
+
+# Should be 0
+print me.getClassiness()
+
+me.addItem("tophat")
+# Should be 2
+print me.getClassiness()
+
+me.addItem("bowtie")
+me.addItem("jacket")
+me.addItem("monocle")
+# Should be 11
+print me.getClassiness()
+
+me.addItem("bowtie")
+# Should be 15
+print me.getClassiness()
